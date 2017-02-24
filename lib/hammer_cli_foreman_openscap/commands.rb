70829d327e177976bedefa669f6fb5dc0b2d4289
Ondrej Prazak <oprazak@redhat.com>
2017-02-24 10:37:32 +0100

    Fixes #18659 - Command for report download in HTML

--- lib/hammer_cli_foreman_openscap/commands.rb
+++ lib/hammer_cli_foreman_openscap/commands.rb
@@ -39,7 +39,7 @@ module HammerCLIForemanOpenscap
     include HammerCLIForemanOpenscap::ResolverCommons
   end
 
-  class DownloadCommand < HammerCLIForeman::Command
+  class DownloadCommand < HammerCLIForeman::SingleResourceCommand
     include HammerCLIForemanOpenscap::ResolverCommons
     action :download
 
@@ -51,13 +51,31 @@ module HammerCLIForemanOpenscap
       { :response => :raw }
     end
 
+    def validate_options
+      super
+      validator.option(:option_path).required
+    end
+
+    def success_message
+      super + @filepath
+    end
+
+    option "--path", "PATH", _("Path to a directory where downloaded file will be saved"),
+        :attribute_name => :option_path
+
     def print_data(response)
+      if response.body.empty?
+        print_message "Response success but empty body was returned - file was not saved"
+        return
+      end
       # get file name from header, remove leading and trailing quotes
       filename = response.headers[:content_disposition].match(/filename=(.*)/)[1].chop.reverse.chop.reverse
       path = option_path.dup || '.'
       path << '/' unless path.end_with? '/'
       raise HammerCLIForemanOpenscap::DownloadError.new "Cannot save file: #{path} does not exist" unless File.directory?(path)
-      File.write(path + filename, response.body)
+      @filepath = path + filename
+      File.write(@filepath, response.body)
+      print_success_message(response)
     end
   end
 end
